ae15e6029db25d0264d454d49e16a4cd3774b5fd
navanjani <navanjani@outlook.com>
2022-07-11 14:56:52 +0200

    change the table name in seed down

--- seeders/20220711120738-some-users.js
+++ seeders/20220711120738-some-users.js
@@ -27,6 +27,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete("People", null, {});
+    await queryInterface.bulkDelete("users", null, {});
   },
 };
